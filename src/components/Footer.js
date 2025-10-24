cdf6ed90a918fc837784943f61add9528a6cb7c7
Rupesh Prajapat <rp15498@gmail.com>
2025-10-24 23:03:01 +0530

    Added footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,11 @@
 import { FOOTER_TEXTS } from "../textHelper/i18n";
+import "./Footer.css";
 
 function Footer() {
     const { copyright } = FOOTER_TEXTS;
 
     return (
-        <footer
-            style={{
-                textAlign: "center",
-                padding: "18px 0",
-                background: "rgba(24, 28, 36, 0.98)",
-                fontSize: "1rem",
-                borderTop: "1px solid #23283b",
-                marginTop: "32px",
-            }}
-        >
+        <footer>
             <p>{copyright}</p>
         </footer>
     );
